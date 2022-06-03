0dbac26b7e74bcf97450215c0be0be7d42374bbd
CrystalBlinky <97834261+CrystalBlinky@users.noreply.github.com>
2022-06-03 11:43:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function bodyload()
+{
+  document.getElementById("changetheme").style.fontSize="20px";
+  document.getElementById("changetheme").style.backgroundColor="White";
+  document.getElementById("changetheme").style.borderRadius="20px";
+  document.getElementById("changetheme").style.color="#f4a7bb";
+  document.getElementById("changetheme").style.fontFamily="Times";
+}
+function changetheme()
+{
+  document.getElementById("anchorTag").style.color="Black";
+  document.getElementById("anchorTag").style.borderColor="Black";
+  document.getElementById("body").style.backgroundColor="#f4a7bb";
+  document.getElementById("changetheme").style.backgroundColor="#f4a7bb";
+document.getElementById("changetheme").style.color="black";
+  document.getElementById("changetheme").style.borderColor="black";
+}
+function ChangeImageOver()
+{
+  document.getElementById("Lisa").src="https://i.pinimg.com/originals/38/c5/84/38c584f92491691e9f821b74c7655045.jpg";
+}
+function ChangeImageLeave()
+{
+  document.getElementById("Lisa").src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F6155d07b6a4507502c0fa3d3%2FCelebrity-Sightings-At-Incheon-Airport%2F960x0.jpg%3FcropX1%3D0%26cropX2%3D2386%26cropY1%3D86%26cropY2%3D1677";
+}
